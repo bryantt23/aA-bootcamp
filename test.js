a2b8f4ce999edeccb3fc378ef08221694e220b4d
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-26 22:29:06 -0800

    part 1

--- test.js
+++ test.js
@@ -3,7 +3,61 @@ class Bootcamp {
     this.name = name;
     this.slogan = slogan;
     this.studentCapacity = studentCapacity;
-    this.bark = bark;
-    this.favoriteFoods = favoriteFoods;
+    this.teachers = [];
+    this.students = [];
+    this.grades = {};
+  }
+
+  getGrades(student) {
+    if (!this.grades[student]) {
+      return [];
+    }
+    return this.grades[student];
+  }
+
+  addGrades(name, grade) {
+    this.grades[name].push(grade);
+  }
+
+  hire(teacher) {
+    this.teachers.push(teacher);
+  }
+
+  enroll(student) {
+    if (this.students.length < this.studentCapacity) {
+      this.students.push(student);
+      return true;
+    }
+    return false;
+  }
+
+  isEnrolled(student) {
+    return this.students.includes(student);
   }
 }
+
+const bootcamp = new Bootcamp(
+  'Map Academy',
+  'Anyone can be a cartographer.',
+  6
+);
+
+console.log(JSON.stringify(bootcamp));
+console.log(bootcamp.getGrades('Alice', '[]'));
+console.log(bootcamp.getGrades('Bob', '[]'));
+bootcamp.grades['Alice'] = 42;
+console.log(bootcamp.getGrades('Alice', '[42]'));
+console.log(bootcamp.getGrades('Bob', '[]'));
+bootcamp.hire('Jeff');
+bootcamp.hire('Matthias');
+console.log(bootcamp.enroll('Alice'));
+console.log(bootcamp.enroll('Bob'));
+console.log(bootcamp.enroll('Alice2'));
+console.log(bootcamp.enroll('Bob2'));
+console.log(bootcamp.enroll('Alice3'));
+console.log(bootcamp.enroll('Bob3'));
+console.log(bootcamp.enroll('Alice4'));
+console.log(bootcamp.enroll('Bob4'));
+console.log(bootcamp.isEnrolled('Alice'), 'true');
+console.log(bootcamp.isEnrolled('Bobxyz'), 'false');
+console.log(JSON.stringify(bootcamp));
