a7973d7631201f092e8aef96eb251f8f5d4b7863
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-26 22:00:15 -0800

    Initial

---
+++ test.js
@@ -0,0 +1,9 @@
+class Bootcamp {
+  constructor(name, slogan, studentCapacity) {
+    this.name = name;
+    this.slogan = slogan;
+    this.studentCapacity = studentCapacity;
+    this.bark = bark;
+    this.favoriteFoods = favoriteFoods;
+  }
+}
